7565b5ed4974d15818a2d9e6542c55a474192584
PuongDI <87972750+phuongduynguyen@users.noreply.github.com>
2022-01-05 17:36:58 +0700

    Add files via upload

---
+++ DeepSleep_TimerToSleep_EXTI/DeepSleep_TimerToSleep_EXTI.ino
@@ -0,0 +1,82 @@
+// https://vidieukhien.xyz/2020/02/16/lap-trinh-esp32-voi-arduino-esp32-timer-interrupt/
+//https://randomnerdtutorials.com/esp32-external-wake-up-deep-sleep/
+
+// EXTI to wake up GPIO33 D33
+
+
+#define BUTTON_PIN_BITMASK 0x200000000 // 2^33 in hex
+
+RTC_DATA_ATTR int bootCount = 0;
+
+volatile int interruptCounter;
+int totalInterruptCounter = 0;
+
+hw_timer_t * timer = NULL;
+portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
+
+void IRAM_ATTR onTimer() {
+  portENTER_CRITICAL_ISR(&timerMux);
+  esp_deep_sleep_start();
+  portEXIT_CRITICAL_ISR(&timerMux);
+
+}
+/*
+Method to print the reason by which ESP32
+has been awaken from sleep
+*/
+void print_wakeup_reason(){
+  esp_sleep_wakeup_cause_t wakeup_reason;
+
+  wakeup_reason = esp_sleep_get_wakeup_cause();
+
+  switch(wakeup_reason)
+  {
+    case ESP_SLEEP_WAKEUP_EXT0 : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
+    case ESP_SLEEP_WAKEUP_EXT1 : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
+    case ESP_SLEEP_WAKEUP_TIMER : Serial.println("Wakeup caused by timer"); break;
+    case ESP_SLEEP_WAKEUP_TOUCHPAD : Serial.println("Wakeup caused by touchpad"); break;
+    case ESP_SLEEP_WAKEUP_ULP : Serial.println("Wakeup caused by ULP program"); break;
+    default : Serial.printf("Wakeup was not caused by deep sleep: %d\n",wakeup_reason); break;
+  }
+}
+
+void setup(){
+  Serial.begin(115200);
+  delay(1000); //Take some time to open up the Serial Monitor
+
+  //Increment boot number and print it every reboot
+  ++bootCount;
+  Serial.println("Boot number: " + String(bootCount));
+
+  //Print the wakeup reason for ESP32
+  print_wakeup_reason();
+  
+
+  /*
+  First we configure the wake up source
+  We set our ESP32 to wake up for an external trigger.
+  There are two types for ESP32, ext0 and ext1 .
+  ext0 uses RTC_IO to wakeup thus requires RTC peripherals
+  to be on while ext1 uses RTC Controller so doesnt need
+  peripherals to be powered on.
+  Note that using internal pullups/pulldowns also requires
+  RTC peripherals to be turned on.
+  */
+  esp_sleep_enable_ext0_wakeup(GPIO_NUM_33,0); //1 = High, 0 = Low
+
+  timer = timerBegin(0, 80, true);
+  timerAttachInterrupt(timer, &onTimer, true);
+  timerAlarmWrite(timer, 30000000, true);
+  timerAlarmEnable(timer);
+  //If you were to use ext1, you would use it like
+  //esp_sleep_enable_ext1_wakeup(BUTTON_PIN_BITMASK,ESP_EXT1_WAKEUP_ANY_HIGH);
+
+  //Go to sleep now
+  Serial.println("Going to sleep now");
+
+}
+
+void loop(){
+  Serial.println(String(totalInterruptCounter++));
+  delay(1000);
+}
